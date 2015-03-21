60073995ff48d528f0cbca9dcc0e8425e420598f
Razi <rrshaban@users.noreply.github.com>
2015-03-21 00:45:14 -0400

    implement user login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+
+    # logs in the given user
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+end
