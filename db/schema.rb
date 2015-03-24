3d670eb1a7db0b6c3f8343431c392801b43f26e1
Razi <rrshaban@users.noreply.github.com>
2015-03-24 11:07:55 -0400

    admin user fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322205351) do
+ActiveRecord::Schema.define(version: 20150324145842) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150322205351) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name:  "Studious Swattie",
              email: "example@swarthmore.edu",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
