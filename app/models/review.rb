e3c84bbf54803c05538a29682f48a24af43a7618
Razi <rrshaban@users.noreply.github.com>
2015-03-24 16:24:02 -0400

    basic review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
+  validates :user_id, presence: true
+  validates :content, presence: true
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:razi)
+    # code is bad below
+    @review = Review.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @review.valid?
+  end
+
+  test "user id should be present" do
+    @review.user_id = nil
+    assert_not @review.valid?
+  end
+
+
+  test "content should be present " do
+    @review.content = "   "
+    assert_not @review.valid?
+  end
+
 end
