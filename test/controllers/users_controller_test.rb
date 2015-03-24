b70bfcf2d350e27c3401bd65a8ee20a09cac423a
Razi <rrshaban@users.noreply.github.com>
2015-03-24 10:15:20 -0400

    archer 'other_user' testing for authentication

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,29 +1,42 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionController::TestCase
+class UsersControllerTest < ActionController::TestCase 
+
+  def setup
+    @user       = users(:razi)
+    # @other_user = users(:archer)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  def setup
-    @user       = users(:razi)
-    @other_user = users(:archer)
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
+  test "should redirect update when not logged in" do
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert_redirected_to root_url
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
-  test "should not allow the admin attribute to be edited via the web" do
-    log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch :update, id: @other_user, user: { password:              FILL_IN,
-                                            password_confirmation: FILL_IN,
-                                            admin: FILL_IN }
-    assert_not @other_user.FILL_IN.admin?
-  end
+  # test "should redirect update when logged in as wrong user" do
+  #   log_in_as(@other_user)
+  #   patch :update, id: @user, user: { name: @user.name, email: @user.email }
+  #   assert_redirected_to root_url
+  # end
+
+  # test "should not allow the admin attribute to be edited via the web" do
+  #   log_in_as(@other_user)
+  #   assert_not @other_user.admin?
+  #   patch :update, id: @other_user, user: { password:              FILL_IN,
+  #                                           password_confirmation: FILL_IN,
+  #                                           admin: FILL_IN }
+  #   assert_not @other_user.FILL_IN.admin?
+  # end
 
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name:  "",
@@ -17,6 +18,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
