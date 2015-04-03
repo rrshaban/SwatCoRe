f6b061d9d2b5568613ef5b0bfe3827179d8fb18b
Razi <rrshaban@users.noreply.github.com>
2015-04-03 12:22:54 -0400

    must login before making new course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,5 @@
 class CoursesController < ApplicationController
+  before_action :logged_in_user
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   # GET /courses
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user
   
   
   def create
