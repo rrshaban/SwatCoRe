8004501c2f46fc87a671bace35e658c40ee80788
Razi <rrshaban@users.noreply.github.com>
2015-04-04 16:46:35 -0400

    delete migrations

--- db/migrate/20150403195540_add_professor_id_to_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddProfessorIdToCourses < ActiveRecord::Migration
-  def change
-    add_reference :courses, :professor, index: true, foreign_key: true
-  end
-end
--- db/migrate/20150404025611_create_departments.rb
+++
@@ -1,9 +0,0 @@
-class CreateDepartments < ActiveRecord::Migration
-  def change
-    create_table :departments do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
