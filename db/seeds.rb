ed04a9588f54c4f87ef9217db7f883697f494c6e
Razi <rrshaban@users.noreply.github.com>
2015-04-05 00:45:01 -0400

    seems to send mail in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,39 +17,39 @@ Department.create!(name: "Computer Science")
 Professor.create!(name: "Ameet Soni",
                   department_id: 1)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example#{n+1}@swarthmore.edu"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example#{n+1}@swarthmore.edu"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password)
+# end
 
-Course.create!( name:        "Introduction to Computer Science",
-                department_id:  1,
-                professor_id:   1,
-                crn:         "CS021")
+# Course.create!( name:        "Introduction to Computer Science",
+#                 department_id:  1,
+#                 professor_id:   1,
+#                 crn:         "CS021")
 
-Course.create!( name:        "Introduction to Computer Systems",
-                department_id:  1,
-                professor_id:   1,
-                crn:         "CS031")
+# Course.create!( name:        "Introduction to Computer Systems",
+#                 department_id:  1,
+#                 professor_id:   1,
+#                 crn:         "CS031")
 
-Course.create!( name:        "Data Structures and Algorithms",
-                department_id:  1,
-                professor_id:   1,
-                crn:         "CS035")
+# Course.create!( name:        "Data Structures and Algorithms",
+#                 department_id:  1,
+#                 professor_id:   1,
+#                 crn:         "CS035")
 
 
-courses = Course.order(:created_at).take(3)
-10.times do
-  content = Faker::Lorem.sentence(5)
-  courses.each { |course| course.reviews.create!(content: content, 
-                                                    user: User.find_by(id: 1), 
-                                                    clarity: 5,
-                                                    intensity: 5,
-                                                    worthit: 5) }
-end
+# courses = Course.order(:created_at).take(3)
+# 10.times do
+#   content = Faker::Lorem.sentence(5)
+#   courses.each { |course| course.reviews.create!(content: content, 
+#                                                     user: User.find_by(id: 1), 
+#                                                     clarity: 5,
+#                                                     intensity: 5,
+#                                                     worthit: 5) }
+# end
 
