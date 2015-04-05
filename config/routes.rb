e3082dfe1b02f9a375c57bba31294b099187fdc3
Razi <rrshaban@users.noreply.github.com>
2015-04-04 21:37:48 -0400

    touchup routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,15 @@ Rails.application.routes.draw do
   get 'login'     => 'sessions#new'
   post 'login'    => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
   resources :users
-  resources :courses
-  resources :departments, only: [:index, :show, :new, :create]
+
+
+  # below is the hierarchy: depts > professors > courses > reviews
+  # once we thoroughly seed depts and professors, we can delete 
+  resources :departments, only: [:index, :show, :new, :create]  # :new and :create
   resources :professors, only: [:index, :show, :new, :create]
+  resources :courses
 
   resources :reviews, only: [:create, :destroy]
 
