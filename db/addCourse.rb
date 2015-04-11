8b0e54213d5b88b59493ee22079efa2d2a53be70
Razi <rrshaban@users.noreply.github.com>
2015-04-11 14:20:28 -0400

    modified addCourse

--- db/addCourse.rb
+++ db/addCourse.rb
@@ -7,10 +7,12 @@ courseList.each{|course|
 	courseName = course['courseName']
 	courseDept = course['dept']
 	courseId = course['courseId']
-	puts "#{courseName}\t#{courseDept}-#{courseId}"
+	# puts "#{courseName}\t#{courseDept}-#{courseId}"
+
+
+  # This might want to work, but we'll need to add all the depts and profs first
+  Course.create!( name:        coursename,
+              department:  courseDept,
+              crn:         courseId)
+  Course.save
 }
-
-
-Course.create!( name:        coursename,
-                department:  courseDept,
-                crn:         courseId)
