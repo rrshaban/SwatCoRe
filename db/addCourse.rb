ebed574cfd2f2f2fdc1f70e4590479b6888c8bdd
Razi <rrshaban@users.noreply.github.com>
2015-04-14 11:23:27 -0400

    testing out add-course

--- db/addCourse.rb
+++ db/addCourse.rb
@@ -4,63 +4,65 @@ hash = JSON.parse(file)
 
 courseList = hash['results']
 
-departmentsArray = Array.new 
-professorsArray = Array.new
-courseList.each{|course|
+i = 0
+depts = Hash.new 
+profs = Hash.new
+
+courseList.each{ |course|
+  i += 1
 	courseDept = course['dept']
 	courseProf = course['profFirstName'] + ' ' + course['profLastName']
 
-if !departmentsArray.include?(courseDept)
-	departmentsArray.push(courseDept)
-end
+  if !depts.include?(courseDept)
+  	depts[courseDept] = 1
+  else
+    depts[courseDept] += 1
+  end
 
-if !professorsArray.include?(courseProf)
-	newArray = [courseProf, courseDept]
-	professorsArray.push(newArray)
-end
+  if !profs.include?(courseProf)
+    # what if more courses are listed for a given prof?
+  	profs[courseProf] = 1
+  else
+    profs[courseProf] += 1
+  end
 }
 
-departmentsArray.each{|dept|
-	Department.create!(name: dept)
-}
-professorsArray.each{|prof|
-	dept = departments.find_by(name:prof[1]).deptartment_id
-	Professor.create!( name: prof[0],
-		  department_id: dept )
-}
+puts depts
+puts profs
+puts i
 
-courseList.each{|course|
-	courseName = course['courseName']
-	courseDept = course['dept']
-	courseId = course['courseId']
-<<<<<<< HEAD
-	courseProf = course['profFirstName'] + ' ' + course['profLastName']
-        courseType = course['courseType']
-	credit = course['credit']
-	division = course['division']
-	hasLab = course['hasLab']
-	isFYS = course['isFYS']
-	isWritingCourse = course['isWritingCourse']
-	couseSummary = course['summary']
-	#puts "#{courseName}\t#{courseDept}-#{courseId}"
+# departmentsArray.each{ |dept|
+# 	Department.create!(name: dept)
+# }
 
-	dept = departments.find_by(name: courseDept).deptartment_id
-	prof = professors.find_by(name: courseProf).professor_id
-=======
-	# puts "#{courseName}\t#{courseDept}-#{courseId}"
->>>>>>> upstream/master
+# professorsArray.each{ |prof|
+# 	dept = departments.find_by(name:prof[1]).department_id
+# 	Professor.create!( name: prof[0],
+# 		  department_id: dept )
+# }
 
-	Course.create!( name:        coursename,
-               department_id:        dept,
-                professor_id:        prof,
-		       	 crn:        courseId ) 
+# courseList.each{ |course|
+# 	courseName = course['courseName']
+# 	courseDept = course['dept']
+# 	courseId = course['courseId']
 
-<<<<<<< HEAD
-=======
-  # This might want to work, but we'll need to add all the depts and profs first
-  Course.create!( name:        coursename,
-              department:  courseDept,
-              crn:         courseId)
-  Course.save
->>>>>>> upstream/master
-}
+# 	courseProf = course['profFirstName'] + ' ' + course['profLastName']
+#   courseType = course['courseType']
+# 	credit = course['credit']
+# 	division = course['division']
+# 	hasLab = course['hasLab']
+# 	isFYS = course['isFYS']
+# 	isWritingCourse = course['isWritingCourse']
+# 	couseSummary = course['summary']
+# 	#puts "#{courseName}\t#{courseDept}-#{courseId}"
+
+# 	dept = departments.find_by(name: courseDept).department_id
+# 	prof = professors.find_by(name: courseProf).professor_id
+
+# 	Course.create!( name:        coursename,
+#                department_id:        dept,
+#                 professor_id:        prof,
+# 		       	 crn:        courseId ) 
+
+
+# }
