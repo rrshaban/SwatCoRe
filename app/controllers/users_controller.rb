4c59d79605cab8c594cc91c76cf3a9fa2cd40cd4
Razi <rrshaban@users.noreply.github.com>
2015-04-23 00:04:40 -0400

    now can destroy a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
   before_action :admin_user, only: [:index]
+  before_action :set_user
 
   def show
-    @user = User.find(params[:id])
     @reviews = @user.reviews
   end
 
   def index
     @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
