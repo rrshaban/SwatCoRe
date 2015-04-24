fdfb63703e4702f236baf48eb651860d9f8eab84
Razi <rrshaban@users.noreply.github.com>
2015-04-23 22:16:37 -0400

    form now works, shows results, and flashes when no results found

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,23 @@
 class SearchController < ApplicationController
+  # include pg_search
+
   def form
     # search queries come here
   end
 
   def search
-    # search results reported here
+    query = q_params
+    @departments = PgSearch.multisearch(query).where(:searchable_type => "Department")
+    @professors = PgSearch.multisearch(query).where(:searchable_type => "Professor")
+    @courses = PgSearch.multisearch(query).where(:searchable_type => "Course")
+
+    if @departments.none? and @professors.none? and @courses.none?
+      redirect_to search_path, notice: 'No results found. Please double-check your query.'
+    end
   end
+
+  private
+  def q_params
+      params.permit(:search)[:search]
+    end
 end
