9fd90f7a2c42445dfd511199e2a9b833b20f4e3d
Razi <rrshaban@users.noreply.github.com>
2015-05-06 10:29:22 -0400

    fixed so you can submit/update actual name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,29 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
 
   # def after_sign_in_path_for(resource)
   #   resource_path
   # end
+
+  protected
+
+
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
+  end
 
   private 
 
