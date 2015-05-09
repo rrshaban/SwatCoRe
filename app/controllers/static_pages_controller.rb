709c7a93f69d87350e9c5bab5422eb089c0bbb98
Razi <rrshaban@users.noreply.github.com>
2015-05-09 11:54:31 -0400

    front page 10->15

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user! #, :only => [:home, :help, :about, :contact]
   
   def home
-    @recent = Review.order(created_at: :asc).last(10).reverse
-    @active = Review.order(updated_at: :asc).last(10).reverse
+    @recent = Review.order(created_at: :asc).last(15).reverse
+    @active = Review.order(updated_at: :asc).last(15).reverse
     @top = Review.order("cached_votes_score DESC").first
   end
 
