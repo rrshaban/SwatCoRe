5978605386b136b84a3cd0c7272a24b348481c5a
Razi <rrshaban@users.noreply.github.com>
2015-05-15 00:37:51 -0400

    changed action mailer default url away from heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'swatcore.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://www.swatcoursereview.com' }
   
   # ActionMailer::Base.smtp_settings = {
   #   :address        => "smtp.sendgrid.net",
